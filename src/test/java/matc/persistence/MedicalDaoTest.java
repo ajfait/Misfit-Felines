f92cd90833e95ef9e58f1edce4ac79da2a4f0d7c
ajfait <jafait1004@gmail.com>
2025-02-19 21:28:35 -0600

    create eventdaotest and medicaldaotest

---
+++ src/test/java/matc/persistence/MedicalDaoTest.java
@@ -0,0 +1,41 @@
+package matc.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicalDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getByMedicalId() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByPropertyEqual() {
+    }
+
+    @Test
+    void getByPropertyLike() {
+    }
+}
